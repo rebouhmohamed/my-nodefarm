50c8ffa9cbc14d07716106b085c16e0c149abd1c
mohamed rebouh <99739241+rebouhmohamed@users.noreply.github.com>
2022-11-02 16:17:57 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //server
@@ -9,7 +10,8 @@ const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
 const tempProduct = fs.readFileSync('./templates/template-product.html', 'utf-8');
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
-
+const slugs = dataObj.map(el =>slugify(el.productName,{lower:true}));
+console.log(slugs);
 const port = 8000;
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -7,6 +7,7 @@ module.exports = (temp, product) => {
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%SKU%}/g, product.sku);
   
   if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   return output;
